73064c0d7a471d3e616dc2892e853a9955fa3886
Papercraft <papercraft271@gmail.com>
2026-05-26 08:37:14 +0530

    Update CORS for Netlify

--- src/index.ts
+++ src/index.ts
@@ -42,10 +42,13 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://paptrix.netlify.app',
+    process.env.FRONTEND_URL || '',
+  ].filter(Boolean),
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Global rate limiter
